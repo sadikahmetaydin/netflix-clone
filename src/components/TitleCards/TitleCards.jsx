e9aa7a041c71c7288284628751559c4fb72cdd6f
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-09-01 00:11:26 +0300

    Netflix Clone Project Final

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
 
-const TitleCards = () => {
-  return (
-    <div className='titlecards'>
+import cards_data from '../../assets/cards/Cards_data'
 
+const TitleCards = ({title, category}) => {
+
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  }
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, [])
+
+  return (
+    <div className='title-cards'>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+
+      <div className="card-list" ref={cardsRef}>
+        {
+          cards_data.map((card, index) => {
+            return (
+              <div className="card" key={index}>
+                <img src={card.image} alt="" />
+                <p>{card.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
